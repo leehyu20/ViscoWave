ebcef3823b2ee42660a9828bc842169394ed3132
Hyung S. Lee <47827262+leehyu20@users.noreply.github.com>
2021-10-12 10:53:29 -0500

    Add files via upload

---
+++ ViscoWave Source Code/Relaxation_Sig_to_Prony/Relaxation_Sig_to_Prony.h
@@ -0,0 +1,4 @@
+// Relaxation_Sig_to_Prony.h
+
+// Returns Prony Coefficients from Sigmoidal Coefficients for Relaxation Modulus
+extern __declspec(dllexport) int Relaxation_Sig_to_Prony(double* Prony, double* Sigmoid, int Num_Sigmoid);
