0d45cc3f5182ebc2380409aa27d90b7d2f39ac8c
Hyung S. Lee <47827262+leehyu20@users.noreply.github.com>
2022-02-24 16:02:28 -0600

    Delete ViscoWave Source Code directory

--- ViscoWave Source Code/ViscoWave/ViscoWave.h
+++
@@ -1,7 +0,0 @@
-// ViscoWave.h
-
-// Returns ViscoWave Deflections
-extern __declspec(dllexport) int ViscoWave(double* displacement, double* Sigmoid, double* Pavement,
-					double Load_Pressure, double Load_Radius, double* Sensor_Location, double* Time,
-					double* Timehistory, double dt, int num_prony_elements, int Num_Pavt_Layers, int Num_Sensors, 
-					int Num_Time, int Num_VE_Layer);
